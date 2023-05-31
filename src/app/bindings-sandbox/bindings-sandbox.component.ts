9c290a9c41dd9c3f382cabbf9bd9fd324a947d3a
alopezo <8TPFdVPnehjv4d>
2023-05-31 18:45:48 -0300

    Output in sandbox enabled

--- src/app/bindings-sandbox/bindings-sandbox.component.ts
+++ src/app/bindings-sandbox/bindings-sandbox.component.ts
@@ -4,7 +4,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatExpansionPanel } from '@angular/material/expansion';
 import { TerminologyService } from '../services/terminology.service';
 import { lastValueFrom, map } from 'rxjs';
-
+import { saveAs } from 'file-saver';
+import { Clipboard } from '@angular/cdk/clipboard';
 @Component({
   selector: 'app-bindings-sandbox',
   templateUrl: './bindings-sandbox.component.html',
@@ -13,42 +14,46 @@ import { lastValueFrom, map } from 'rxjs';
 export class BindingsSandboxComponent {
   @ViewChild('newPanel') newPanel!: MatExpansionPanel;
 
-  bindings: any[] = [
-    // {
-    //   title: 'Clinical data entry form',
-    //   type: 'Title',
-    //   ecl: ``,
-    //   value: '',
-    //   note: ''
-    // },
-    // {
-    //   title: 'Diagnosis 1',
-    //   type: 'Autocomplete',
-    //   ecl: `<< 404684003 |Clinical finding (finding)|`,
-    //   value: '',
-    //   note: 'The diagnosis for the clinical encounter.'
-    // },
-    // {
-    //   title: 'Appendicitis type',
-    //   type: 'Select (Single)',
-    //   ecl: `<< 196781001 |Acute appendicitis with peritonitis (disorder)|`,
-    //   value: '',
-    //   note: 'The type of appendicitis.'
-    // },
-    // {
-    //   title: 'Appendicitis type (M)',
-    //   type: 'Select (Multiple)',
-    //   ecl: `<< 196781001 |Acute appendicitis with peritonitis (disorder)|`,
-    //   value: '',
-    //   note: 'The type of appendicitis.'
-    // },
-    // {
-    //   title: 'Appendicectomy type',
-    //   type: 'Options',
-    //   ecl: `<< 174036004 |Emergency appendectomy (procedure)|`,
-    //   value: '',
-    //   note: 'The type of appendicectomy.'
-    // }
+  bindings: any[] = [];
+  output: any = {};
+  outputStr: string = '{}';
+
+  example1 = [
+    {
+      title: 'Appendicitis data entry form (example)',
+      type: 'Title',
+      ecl: ``,
+      value: '',
+      note: ''
+    },
+    {
+      title: 'Diagnosis (type of appendicitis)',
+      type: 'Autocomplete',
+      ecl: `<< 74400008 |Appendicitis|`,
+      value: '',
+      note: 'An autocomplete field to select between all descendants of appendicitis.'
+    },
+    {
+      title: 'Appendicitis type',
+      type: 'Select (Single)',
+      ecl: `<< 196781001 |Acute appendicitis with peritonitis (disorder)|`,
+      value: '',
+      note: 'A single selection dropdown field for a shorter selection of the type of appendicitis.'
+    },
+    {
+      title: 'Appendicitis type (M)',
+      type: 'Select (Multiple)',
+      ecl: `<< 196781001 |Acute appendicitis with peritonitis (disorder)|`,
+      value: '',
+      note: 'A multiple selection dropdown field for a shorter selection of the type of appendicitis.'
+    },
+    {
+      title: 'Appendicectomy type',
+      type: 'Options',
+      ecl: `<< 174036004 |Emergency appendectomy (procedure)|`,
+      value: '',
+      note: 'A radio buttons selector for the type of appendicectomy procedure.'
+    }
   ];
 
   newBindingForm = new FormGroup({
@@ -66,7 +71,7 @@ export class BindingsSandboxComponent {
 
   controlTypes = ['Autocomplete', 'Select (Single)', 'Select (Multiple)', 'Options', 'Title'];
 
-  constructor(private terminologyService: TerminologyService) { }
+  constructor(private terminologyService: TerminologyService, private clipboard: Clipboard) { }
 
   async addBinding() {
     this.newBindingForm.markAllAsTouched();
@@ -164,6 +169,53 @@ export class BindingsSandboxComponent {
 
   optionSelected(title: string, event: any) {
     console.log(title, event)
+    this.output[title] = event;
+    this.outputStr = JSON.stringify(this.output, null, 2);
+  }
+
+  loadExample1() {
+    this.bindings = this.example1;
+  }
+
+  clear() {
+    this.bindings = [];
+    this.clearOutput();
+  }
+
+  clearOutput() {
+    this.output = {};
+    this.outputStr = '{}';
+
+  }
+
+  saveForm() {
+    var blob = new Blob([JSON.stringify(this.bindings, null, 2)], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, "Sandbox-form.json");
+  }
+
+  saveOutput() {
+    var blob = new Blob([this.outputStr], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, "AllergyIntolerance.json");
+  }
+
+  copyOutputToClipboard(text: string) {
+    this.clipboard.copy(text);
+  }
+
+  uploadFile(event: any) {
+    if (event.target.files.length !== 1) {
+      console.error('No file selected');
+    } else {
+      const reader = new FileReader();
+      reader.onloadend = (e) => {
+        if (reader.result) {
+          const uploadedVersion = JSON.parse(reader.result?.toString());
+          this.bindings = uploadedVersion;
+          this.clearOutput();
+        }
+      };
+      reader.readAsText(event.target.files[0]);
+    }
   }
 
 }
