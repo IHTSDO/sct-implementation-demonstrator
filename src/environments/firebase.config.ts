7f463ab7276fd58c75a6e75eb7f014a73ba8d5fe
Alejandro Lopez Osornio <alopez72@gmail.com>
2024-03-27 18:16:51 -0300

    Adding message explaining public keys

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,3 +1,6 @@
+// These are public Firebase Keys, so they are safe to be shared in the public repository.
+// Learn more: https://firebase.google.com/docs/projects/api-keys
+
 export const firebaseConfig = {
     apiKey: "AIzaSyAFjiIuMBA1IpTrw__WdkQiK5PKht4_go8",
     authDomain: "snoguess-e4d1c.firebaseapp.com",
