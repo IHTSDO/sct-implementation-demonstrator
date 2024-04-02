588ee37b4e6f300a809f39e57ebafd85f43c7af0
Alejandro Lopez Osornio <alopez72@gmail.com>
2024-04-02 17:53:12 -0300

    updating lforms version

--- src/app/questionnaires/questionnaires-main/questionnaires-main.component.ts
+++ src/app/questionnaires/questionnaires-main/questionnaires-main.component.ts
@@ -64,10 +64,10 @@ export class QuestionnairesMainComponent implements OnInit{
 
   loadScript() {
     const script = document.createElement('script');
-    script.src = 'https://clinicaltables.nlm.nih.gov/lforms-versions/34.3.1/webcomponent/lhc-forms.js'; // Replace with the actual URL of your JS library
+    script.src = 'https://clinicaltables.nlm.nih.gov/lforms-versions/35.0.4/webcomponent/lhc-forms.js'; // Replace with the actual URL of your JS library
     script.onload = () => {
       const script2 = document.createElement('script');
-      script2.src = 'https://clinicaltables.nlm.nih.gov/lforms-versions/34.0.0/fhir/R4/lformsFHIR.min.js'; // Replace with the actual URL of your JS library
+      script2.src = 'https://clinicaltables.nlm.nih.gov/lforms-versions/35.0.4/fhir/R4/lformsFHIR.min.js'; // Replace with the actual URL of your JS library
       script2.onload = () => {
         // Script has loaded
         // Initialize or use the library here, if necessary
