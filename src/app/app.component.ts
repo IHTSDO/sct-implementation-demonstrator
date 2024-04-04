7e451cd5330458089117ca3556b1c28195b45060
Alejandro Lopez Osornio <alopez72@gmail.com>
2024-04-04 10:03:57 -0300

    Removing comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,17 +27,6 @@ export class AppComponent {
   selectedServer = this.fhirServers[1];
   embeddedMode: boolean = false;
 
-  // firebaseConfig = {
-  //   apiKey: "AIzaSyAFjiIuMBA1IpTrw__WdkQiK5PKht4_go8",
-  //   authDomain: "snoguess-e4d1c.firebaseapp.com",
-  //   projectId: "snoguess-e4d1c",
-  //   storageBucket: "snoguess-e4d1c.appspot.com",
-  //   messagingSenderId: "289449411190",
-  //   appId: "1:289449411190:web:49ff68411f9e5148f11557"
-  // };
-
-  // app = initializeApp(this.firebaseConfig);
-
   constructor( private codingSpecService: CodingSpecService, 
     public excelService: ExcelService, 
     private terminologyService: TerminologyService, 
