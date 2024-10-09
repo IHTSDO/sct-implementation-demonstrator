0dc6c2fd43e3252890fa412aaddb3d13224b5039
alopezo <alopez72@gmail.com>
2024-10-09 08:16:00 -0300

    Passing query params from home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MenuService } from '../services/menu.service';
 
 @Component({
@@ -10,7 +10,7 @@ import { MenuService } from '../services/menu.service';
 export class HomeComponent implements OnInit{
   demos: any[] = [];
 
-  constructor(public router: Router, private menuService: MenuService) { }
+  constructor(public router: Router, public route: ActivatedRoute, private menuService: MenuService) { }
 
   ngOnInit(): void {
     this.demos = this.menuService.getDemos();
@@ -18,7 +18,14 @@ export class HomeComponent implements OnInit{
 
   navigate(demo: any) {
     if (demo.type === 'internal') {
-      this.router.navigate([demo.url], { queryParams: demo.queryParams });
+      let queryParams = demo.queryParams;
+      let currentParams = this.route.snapshot.queryParams;
+      if (queryParams) {
+        queryParams = { ...currentParams, ...queryParams };
+      } else {
+        queryParams = currentParams;
+      }
+      this.router.navigate([demo.url], { queryParams: queryParams });
     } else {
       this.openInNewTab(demo.url);
     }
