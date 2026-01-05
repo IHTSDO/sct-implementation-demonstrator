8174ebe3ee94b8decbe7f3fe21c29c601a12e426
Alejandro Lopez Osornio <alejandro.lopezosornio@gmail.com>
2026-01-05 11:06:40 -0300

    Angular 20 upgrade

--- docs/chunk-RUIJBL2N.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-PROPCZWA.js";import"./chunk-TSRGIXR5.js";export default a();
---
+++ docs/chunk-YNCDII73.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-SOCBGKAB.js";import"./chunk-TSRGIXR5.js";export default a();
