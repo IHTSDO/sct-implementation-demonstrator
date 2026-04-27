8fff16b53faf438b665529245318715ebe6bc8bf
alopezo <alopez72@gmail.com>
2026-04-27 13:42:15 -0300

    Defining prod values for servers

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,13 @@
 import { FhirServer } from './fhir-server.interface';
 
 export const environment = {
-  production: true,
+  production: false,
   fhirServers: [
-    { name: 'SNOMED Dev IS',         url: 'https://dev-is-browser.ihtsdotools.org/fhir' },
-    { name: 'SNOMED Public',         url: 'https://snowstorm.ihtsdotools.org/fhir' },
-    { name: 'SNOMED Dev 2',          url: 'https://snowstorm-temp.kaicode.io/fhir' },
-    { name: 'SNOMED Lite Demo',      url: 'https://implementation-demo.snomedtools.org/snowstorm-lite/fhir' },
-    { name: 'LOINC Ontology Server', url: 'https://browser.loincsnomed.org/fhir' },
-    { name: 'Ontoserver',            url: 'https://r4.ontoserver.csiro.au/fhir' },
-    { name: 'SNOMED Dev 1',          url: 'https://dev-browser.ihtsdotools.org/fhir' },
-    { name: 'Implementation Demo',   url: 'https://implementation-demo.snomedtools.org/fhir' },
+    { name: 'SNOMED Public Snowstorm',         url: 'https://snowstorm.ihtsdotools.org/fhir' },
+    { name: 'Snowstorm Lite Demo',      url: 'https://implementation-demo.snomedtools.org/snowstorm-lite/fhir' },
+    { name: 'LOINC Ontology Snowstorm', url: 'https://browser.loincsnomed.org/fhir' },
+    { name: 'Ontoserver R4 Demo',            url: 'https://r4.ontoserver.csiro.au/fhir' },
+    { name: 'Implementation Demo SnowstormX',   url: 'https://implementation-demo.snomedtools.org/fhir' },
   ] as FhirServer[],
-  defaultFhirServerIndex: 1,
+  defaultFhirServerIndex: 0,
 };
